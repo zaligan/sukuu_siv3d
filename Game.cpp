7fadfc41bf256a08cc815bbd59a48940375a0b92
KaitoIto <kaito.itou08@gmail.com>
2023-10-26 21:36:29 +0900

    帰宅

--- Game.cpp
+++ Game.cpp
@@ -10,10 +10,19 @@ Game::Game(const InitData& init)
 
 void Game::update()
 {
+	pJet_pos = OffsetCircular();
 	if (KeyA.pressed())
 		pJet_pos.x -= 10.0;
 	
 	camera.update();
+	camera.setTargetCenter(pJet_pos);
+}
+
+void Game::draw() const
+{
+	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
+	cat.drawAt(300, 300);
+
 	{
 		// 2D カメラの設定から Transformer2D を作成
 		const auto t = camera.createTransformer();
@@ -22,10 +31,6 @@ void Game::update()
 
 		cat.drawAt(0, 0);
 		pJetTex.drawAt(pJet_pos);
+		
 	}
-}
-
-void Game::draw() const
-{
-	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
 }
