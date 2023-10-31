ee66f85d9c02a9e6e7e8d0555e330f1ac32cfc1a
Iwahara Takaho <g031s020@s.iwate-pu.ac.jp>
2023-11-01 04:44:58 +0900

    タイトル画面編集

--- Title.cpp
+++ Title.cpp
@@ -21,9 +21,12 @@ void Title::draw() const
 {
 	Scene::SetBackground(ColorF{ 0.9, 0.95, 1.0 });
 	titleBackTex.scaled(0.9).drawAt(Scene::Center());
-	button_S.draw(Palette::Orange);
-	button_E.draw(Palette::Skyblue);
+	button_S.draw();
+	button_E.draw();
 
-	font(U"はじめる").draw(330,355);
-	font(U"おわる").draw(350, 425);
+	button_SP.scaled(0.6).drawAt(400, 380);/*001_02.pngの時はscaled(0.7)*/
+	button_EP.scaled(0.6).drawAt(400, 450);
+
+	font(U"START").draw(340, 350, Palette::White);
+	font(U"EXIT").draw(360, 420, Palette::White);
 }
--- Title.h
+++ Title.h
@@ -13,10 +13,12 @@ public:
 	void draw() const override;
 
 private:
-	RoundRect button_S{ 325,350,150,60,10 };
-	RoundRect button_E{ 325,420,150,60,10 };
+	RoundRect button_S{285,350,230,55,20};
+	RoundRect button_E{ 285,420,230,55,20 };
+	const Texture button_SP{ U"picture/Button/001.png" };
+	const Texture button_EP{ U"picture/Button/005_b.png" };
 
-	Font font{ 35 };
+	Font font{ 40 };
 
 	const Texture titleBackTex{ U"picture/AsteriaSalvare　タイトル画面.jpg" };
 };
