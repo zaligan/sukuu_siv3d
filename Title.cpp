b844b62a19bf594c5f4387f842f5a84617a187b0
Iwahara Takaho <g031s020@s.iwate-pu.ac.jp>
2023-11-05 03:46:52 +0900

    タイトル編集お願いします

--- Title.cpp
+++ Title.cpp
@@ -8,13 +8,17 @@ void Title::update()
 {
 	if (st_Button.leftClicked())
 	{
-		SelectAud.play();
+		SelectAud_Push.play();
 		changeScene(State::Game);
 	}
 	if (ex_Button.leftClicked())
 	{
 		System::Exit();
 	}
+	/*if (st_Button.mouseOver() || ex_Button.mouseOver())カーソルが上に乗った瞬間だけ音を出すにしたい
+	{
+		SelectAud_Move.playOneShot();
+	}*/
 }
 
 //updateの後に実行される、主に描画
